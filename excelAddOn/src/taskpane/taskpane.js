f209cafb49225275ed6a6c015cde99181df0528c
sudha7562 <sudhakumari@microsoft.com>
2022-09-20 18:06:59 +0530

    Excel add-on initialization

---
+++ excelAddOn/src/taskpane/taskpane.js
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license.
+ * See LICENSE in the project root for license information.
+ */
+
+/* global console, document, Excel, Office */
+
+Office.onReady((info) => {
+  if (info.host === Office.HostType.Excel) {
+    document.getElementById("sideload-msg").style.display = "none";
+    document.getElementById("app-body").style.display = "flex";
+    document.getElementById("run").onclick = run;
+  }
+});
+
+// export async function run() {
+//   try {
+//     await Excel.run(async (context) => {
+//       /**
+//        * Insert your Excel code here
+//        */
+//       const range = context.workbook.getSelectedRange();
+
+//       // Read the range address
+//       range.load("address");
+
+//       // Update the fill color
+//       range.format.fill.color = "yellow";
+
+//       await context.sync();
+//       console.log(`The range address was ${range.address}.`);
+//     });
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
+
+
+export async function run() {
+  try {
+    await Excel.run(async (context) => {
+      /**
+       * Insert your Excel code here
+       */
+      const range = context.workbook.getSelectedRange();
+
+      // Read the range address
+      range.load("address");
+
+      // Update the fill color
+      range.format.fill.color = "yellow";
+       fetch('https://swapi.dev/api/people/1')
+      .then(response =>{
+          return response.json();
+      }).then(data =>{
+          console.log(data);
+      })
+
+      await context.sync();
+      console.log(`The range address was ${range.address}.`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
